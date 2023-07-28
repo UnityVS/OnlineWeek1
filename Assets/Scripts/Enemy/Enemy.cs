352b75bb3073fd201bb12a6947ce7ff4a1c39df6
MemorySharp <letovsy@yandex.ru>
2023-07-28 11:53:47 +0300

    clearing

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,9 +5,5 @@ namespace Assets.Scripts.Enemy
 {
     public class Enemy : MonoBehaviour
     {
-        public void SetPosition(Vector3 position)
-        {
-            transform.position = position;
-        }
     }
 }
